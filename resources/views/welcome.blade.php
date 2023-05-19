25a08ddd018653623193e7a8cdfbb0a53e5631be
vanyarajwadevy24 <vanyarajwadevy@gmail.com>
2023-05-19 22:29:00 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Laravel</title>
+</head>
+<body>
+    <div class="container">
+        <table class="table mt-5">
+            <thead>
+              <tr class="table-secondary">
+                <th scope="col">ID</th>
+                <th scope="col">Aksi</th>
+                <th scope="col">Avatar</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/me.jpg" alt="" width="100px"></td>
+                <td>vanya</td>
+                <td>id1@gmail.com</td>
+                <td>085716160088</td>
+                <td>Staff</td>
+              </tr>
+              <tr>
+              <th scope="row">2</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/reza.jpg" alt="" width="100px"></td>
+                <td>reza</td>
+                <td>id2@gmail.com</td>
+                <td>089111222333</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+              <th scope="row">3</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>rizki</td>
+                <td>id3@gmail.com</td>
+                <td>082113456789</td>
+                <td>Staff</td>
+              </tr>
+              <tr>
+              <th scope="row">4</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>jian</td>
+                <td>id4@gmail.com</td>
+                <td>087887877878</td>
+                <td>Staff</td>
+              </tr>
+              <tr>
+              <th scope="row">5</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>heca</td>
+                <td>id5@gmail.com</td>
+                <td>089788889999</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+              <th scope="row">6</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/natta.jpg" alt="" width="100px"></td>
+                <td>natta</td>
+                <td>id6@gmail.com</td>
+                <td>087889899999</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+              <th scope="row">7</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>naura</td>
+                <td>id7@gmail.com</td>
+                <td>089899991111</td>
+                <td>Staff</td>
+              </tr>
+              <tr>
+              <th scope="row">8</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>vania</td>
+                <td>id8@gmail.com</td>
+                <td>089787876551</td>
+                <td>Staff</td>
+              </tr>
+              <tr>
+              <th scope="row">9</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>audy</td>
+                <td>id9@gmail.com</td>
+                <td>078765431456</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+              <th scope="row">10</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icon.jpg" alt="" width="100px"></td>
+                <td>sabil</td>
+                <td>id10@gmail.com</td>
+                <td>021765765896</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+              <th scope="row">11</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="img/icel.jpg" alt="" width="100px"></td>
+                <td>cecel</td>
+                <td>id11@gmail.com</td>
+                <td>089762676767</td>
+                <td>Admin</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+</body>
+</html>
